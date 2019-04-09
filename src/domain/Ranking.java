62f6e779c68659778952b5f0ec466ab18e977b83
Alex Torregrosa <alextorre98@gmail.com>
2019-04-10 00:34:40 +0200

    RankingProblema

--- src/domain/Ranking.java
+++ src/domain/Ranking.java
@@ -1,4 +1,33 @@
 package domain;
 
-abstract class Ranking {
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+interface Ranking {
+    //TODO: Consultar Elvira: Llençar excepcions o consultora existeixUsuari()?
+
+    /**
+     * Obté la puntuació màxima de l'usuari en el ranking.
+     *
+     * @param usuari Usuari a consultar.
+     * @return Puntuació de l'usuari.
+     */
+    String puntuacioUsuari(Usuari usuari);
+
+    /**
+     * Obté la posició de l'usuari en el ranking.
+     *
+     * @param usuari Usuari a consultar.
+     * @return Posició de l'usuari en el ranking.
+     */
+    int posicioUsuari(Usuari usuari);
+
+    /**
+     * Obté la llista d'usuaris i puntuacions del ranking.
+     *
+     * @param n Número de entrades del ranking a obtenir.
+     * @return Llista amb els n millors usuaris i les seves puntuacions, ordenada de millor a pitjor.
+     */
+    ArrayList<Pair<String, String>> getLlistaRanking(int n);
 }
--- src/domain/RankingTotal.java
+++ src/domain/RankingTotal.java
@@ -1,4 +1,22 @@
 package domain;
 
-public class RankingTotal extends Ranking {
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class RankingTotal implements Ranking {
+    @Override
+    public String puntuacioUsuari(Usuari usuari) {
+        return null;
+    }
+
+    @Override
+    public int posicioUsuari(Usuari usuari) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Pair<String, String>> getLlistaRanking(int n) {
+        return null;
+    }
 }
