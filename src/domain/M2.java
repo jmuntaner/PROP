7c3c67d56d2c209233a106c4a3176b593d5dde41
Joan Muntaner <joan.francesc.muntaner@est.fib.upc.edu>
2019-04-12 00:21:32 +0200

    Detall maquines

--- src/domain/M2.java
+++ src/domain/M2.java
@@ -2,7 +2,5 @@ package domain;
 
 public class M2 extends Maquina {
     @Override
-    public Moviment calculaMoviment(Tauler t) {
-        return null;
-    }
+    public int heuristica(char[] llistaPeces) {return 0;}
 }
--- src/domain/Maquina.java
+++ src/domain/Maquina.java
@@ -1,5 +1,22 @@
 package domain;
 
 public abstract class Maquina {
-    public abstract Moviment calculaMoviment(Tauler t);
+    public abstract int heuristica(char[] llistaPeces);
+
+    private int valor_peca(char p) {
+        int v = 0;
+        switch (p) {
+            case 'p': v=1; break;
+            case 'P': v=1; break;
+            case 'r': v=5; break;
+            case 'R': v=5; break;
+            case 'b': v=3; break;
+            case 'B': v=3; break;
+            case 'n': v=3; break;
+            case 'N': v=3; break;
+            case 'q': v=9; break;
+            case 'Q': v=9; break;
+        }
+        return v;
+    }
 }
