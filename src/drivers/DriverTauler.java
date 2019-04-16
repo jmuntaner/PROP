c5d45679a58c0cfdf47d35c4fb06299d706efc26
roger.serrat <roger.serrat@est.fib.upc.edu>
2019-04-16 22:28:38 +0200

    JP Moviment

--- src/drivers/DriverTauler.java
+++ src/drivers/DriverTauler.java
@@ -1,32 +1,97 @@
 package drivers;
 
-public class DriverTauler {
+import domain.Tauler;
 
-    public static void testConstructor() {}
+public class DriverTauler extends GenericDriver{
+    private Tauler t;
 
-    public static void testConstructor2() {}
-
-    public static void testAfegirPeca() {}
-
-    public static void testTreurePeca() {}
-
-    public static void testMou() {}
-
-    public static void testMouInvers() {}
-
-    public static void testObteMovimentsJugador() {}
-
-    public static void testObteMovimentsPeca() {}
-
-    public static void testGetCasella() {}
-
-    public static void testFinalEntradaTauler() {}
-
-    public static void testComprovaSolAux() {}
-
-    public static void main(String [] args) {
-        System.out.println("Driver de tauler");
+    DriverTauler(String [] args) {
+        super(args);
+        opcionsMenu = new String [] {
+                "Test constructor",
+                "Test constructor 2",
+                "test afegir peça",
+                "test treure peça",
+                "test mou",
+                "test mou invers",
+                "test obté moviments jugador",
+                "test obté moviments peça",
+                "test get casella",
+                "test final entrada tauler",
+                "test comprova sol aux"
+        };
     }
 
+    public static void main(String [] args) {
+        DriverTauler driver = new DriverTauler(args);
+        driver.runLoop();
+    }
+
+    @Override
+    void runTest(int option) {
+        if (option!=1 && option!=2 && t==null) {
+            System.out.println("ERROR: TAULER NULL");
+            return;
+        }
+        switch(option) {
+            case 1:
+                testConstructor();
+                break;
+            case 2:
+                testConstructor2();
+                break;
+            case 3:
+                testAfegirPeca();
+                break;
+            case 4:
+                testTreurePeca();
+                break;
+            case 5:
+                testMou();
+                break;
+            case 6:
+                testMouInvers();
+                break;
+            case 7:
+                testObteMovimentsJugador();
+                break;
+            case 8:
+                testObteMovimentsPeca();
+                break;
+            case 9:
+                testGetCasella();
+                break;
+            case 10:
+                testFinalEntradaTauler();
+                break;
+            case 11:
+                testComprovaSolAux();
+                break;
+            default:
+                System.out.println("Test no vàlid");
+        }
+    }
+
+    public void testConstructor() {}
+
+    public void testConstructor2() {}
+
+    public void testAfegirPeca() {}
+
+    public void testTreurePeca() {}
+
+    public void testMou() {}
+
+    public void testMouInvers() {}
+
+    public void testObteMovimentsJugador() {}
+
+    public void testObteMovimentsPeca() {}
+
+    public void testGetCasella() {}
+
+    public void testFinalEntradaTauler() {}
+
+    public void testComprovaSolAux() {}
 
 }
