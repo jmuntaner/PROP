acc0047ff29c94f391ded5baca885c0c5342a76b
Alex Torregrosa <alextorre98@gmail.com>
2019-04-26 01:30:30 +0200

    Scripts de compilació actualitzats

--- compilar.sh
+++ compilar.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Compila codi java
+# (c) 2019 Alex Torregrosa
+if [ ! -d out ]; then
+                mkdir out;
+        fi
 shopt -s globstar;
 javac -d out FONTS/**/**.java -cp FONTS/ -cp EXE/junit-4.12.jar;
--- quinsJPfalten.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in jocsProves/* 
-do
-	if [ ! -s "${f}/jp_in.txt" ]; then
-		echo "$f";
-	fi
-done
